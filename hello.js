b0afa60a1279f0906ead950792c16ef815dbb15b
Devika Anilkumar <47947479+Devikaanilkumar@users.noreply.github.com>
2021-10-21 17:56:38 +0530

    Update hello.js

--- hello.js
+++ hello.js
@@ -1,7 +1,7 @@
 //hello.ts
 var Greeter = /** @class */ (function () {
     function Greeter() {
-        this.greeting = "Devika";
+        this.greeting = "Devika ...";
     }
     Greeter.prototype.greet = function () {
         return "Hello, " + this.greeting;
